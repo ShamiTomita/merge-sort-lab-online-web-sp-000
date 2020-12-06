db977f46a95cf63e2815229dc88d5a7994ebdc3c
Shami Tomita <shamitomita@gmail.com>
2020-12-06 16:26:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+//1. Implement a function that first finds and removes the smallest element in a sorted array.
+//2. Implement the merge function to combine two sorted arrays into one larger array.
+//3. Implement the mergeSort algorithm using the previous functions that you wrote. 
 
+function findMinAndRemoveSorted(array){
+  let min = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i];
+      minIndex = i
+    }
+    array.splice(minIndex, 1)
+    return min;
+  }
+}
+
+function findMinAndRemove(array1, array2){
+  let minfirstHalf = array1[0]
+  let minsecondHalf = array2[0]
+ 
+  if(minfirstHalf < minsecondHalf){
+    return array1.shift()
+  } else {
+    return array2.shift()
+  }
+}
+
+
+function merge(array1, array2){
+  let sorted = []
+  let firstHalf = array1
+  let secondHalf = array2
+  let currentMin;
+  while(firstHalf.length != 0 && secondHalf.length != 0){
+    let currentMin = findMinAndRemove(firstHalf, secondHalf)
+    sorted.push(currentMin)
+  }
+  return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+ function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+ 
+    if(array.length < 2){
+      return array
+    } else {
+      merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+  }
