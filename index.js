f4887d0947821f1aaa728314eff09139d3e49aa5
Shami Tomita <shamitomita@gmail.com>
2020-12-06 16:35:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ function merge(array1, array2){
   while(leftIndex < array1.length && rightIndex <array2.length){
     if (array1[leftIndex] < array2[rightIndex]){
       leftIndex++;
+    }else{
+      
     }
       
     
