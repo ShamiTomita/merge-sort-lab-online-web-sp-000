2970b9ebe6bded7633a6078a1337c1fc5e2c959b
Shami Tomita <shamitomita@gmail.com>
2020-12-06 16:34:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function merge(array1, array2){
   rightIndex = 0
   let currentMin;
   while(leftIndex < array1.length && rightIndex <array2.length){
-    if (array1[leftIndex] < array2[rightIndex]{
-      leftIndex++
+    if (array1[leftIndex] < array2[rightIndex]){
+      leftIndex++;
     }
       
     
