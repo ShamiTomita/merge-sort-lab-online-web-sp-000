fc3cfc5b0df90b51b6b16e3f39ae8aea48ec1d8c
Shami Tomita <shamitomita@gmail.com>
2020-12-06 16:33:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function merge(array1, array2){
   leftIndex = 0
   rightIndex = 0
   let currentMin;
-  while(leftIndex < array1.length ){
+  while(leftIndex < array1.length && rightIndex <){
     let currentMin = findMinAndRemove(firstHalf, secondHalf)
     sorted.push(currentMin)
   }
